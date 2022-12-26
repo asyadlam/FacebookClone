4aa773d90e31e668fe6e7d0f05e0eb1fa58c45da
Asya <abbydlevy@gmail.com>
2022-12-26 11:23:49 +0100

    search bar

--- fb-clone/src/Header.js
+++ fb-clone/src/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import "./Header.css";
-
+import SearchIcon from '@mui/icons-material/Search';
 
 function Header() {
     return <div className="header">
         <div className="header__left">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" alt="" />
             <div className="header__input">
-
+                <SearchIcon />
+                <input type="text" />
             </div>
         </div>
         <div className="header__middle"></div>
