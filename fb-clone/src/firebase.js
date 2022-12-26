bd595341c63aa7fc5630fa6b8be75ad81c7d431f
Asya <abbydlevy@gmail.com>
2022-12-26 01:50:32 +0100

    firebase.js

---
+++ fb-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD17UuZPR_U_oK14h69ZRvh2CsSfCKbbQ0",
+    authDomain: "facebook-c645e.firebaseapp.com",
+    projectId: "facebook-c645e",
+    storageBucket: "facebook-c645e.appspot.com",
+    messagingSenderId: "620598868392",
+    appId: "1:620598868392:web:5d1c3c1a6e6c24d9a7059d",
+    measurementId: "G-M78M7XSNYV"
+  };
