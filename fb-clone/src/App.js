500115559b73c8fa5f1c3cb020912dd3713cfa28
Asya <abbydlevy@gmail.com>
2022-12-26 11:08:43 +0100

    logo

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
     // BEM naming convention
     <div className="app">
       <h1>Facebook Clone</h1>
+
+      {/* Header */}
+      <Header />
+
+
+      {/* App Body */}
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+
     </div>
   );
 }
